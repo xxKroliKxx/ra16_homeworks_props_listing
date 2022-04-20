191ac84e3441fb30281f223aa5e17f82d84f20c7
rka <russkikh_k@ekb.sima-land.ru>
2022-04-21 00:08:53 +0500

    +

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ import Listing from "./Listing";
 let offersList = []
 
 function App() {
-    let [offers, setOffersList] = useState(offersList)
+    let [offers, setOffersList] = useState(getData)
 
-    fetch("./etsy.json").then(async response => {
-            const data = await response.json()
-            setOffersList(data)
-        }
-    )
+    function getData() {
+        fetch("./etsy.json").then(async response => {
+                const data = await response.json()
+                setOffersList(data)
+            }
+        )
+    }
+
+    if (!offers) return <></>
 
     return (
         <div className="item-list"> {
-            offers.map(offer => {
+            offers.
+            filter(offer => offer.state != 'removed').
+            map(offer => {
                 return <Listing
                     key={offer.listing_id}
                     currency_code={offer.currency_code}
-                    url_image={offer.MainImage ? offer.MainImage.url_570xN : ""}
+                    url_image={offer.MainImage.url_570xN}
                     title={offer.title}
                     url={offer.url}
                     price={offer.price}
